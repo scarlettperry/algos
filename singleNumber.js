472692443a768b4789524a313ba5fe5ddf888b9d
scarlettperry <si.perry15@gmail.com>
2018-11-24 14:10:08 -0500

    single number

---
+++ singleNumber.js
@@ -0,0 +1,18 @@
+//[1,2,1]
+
+// function singleNumber(array) {
+//   //check to see if a number has a duplicate
+//   let uniqueInt
+//   let duplicates = []
+// 
+//   for(i=0; i < array.length; i++){
+//     if(array[i] === i){
+//       duplicates.push(i)
+//     }
+//     else{
+//       uniqueInt = i
+//     }
+//   }
+//   console.log(duplicates)
+//   return uniqueInt
+// }
