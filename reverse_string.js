f9aa245dd9e086af26c1a3ca89cf20dc8a3e3724
scarlettperry <si.perry15@gmail.com>
2018-11-24 12:43:43 -0500

    reverse string

---
+++ reverse_string.js
@@ -0,0 +1,12 @@
+let reverseString = function(s) {
+    let newString = s.split("").reverse().join("")
+    return newString
+}
+//reverseString("tacocat")
+
+reverseString2 = (s) => {
+  let newString = s.split("").reverse().join("")
+  return newString
+}
+//reverseString2("mom")
+//reverseString2("scarlett")
---
+++ reverse_string.rb
@@ -0,0 +1,4 @@
+def reverse_string(s)
+  s.reverse
+end
+# reverse_string("scarlett")
