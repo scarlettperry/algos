288785fa819266863d78d53c8ae8dc657d8ae851
scarlettperry <si.perry15@gmail.com>
2018-12-25 16:09:38 -0500

    solution for single number

--- javascript/singleNumber.js
+++ javascript/singleNumber.js
@@ -1,18 +1,15 @@
-//[1,2,1]
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
-// function singleNumber(array) {
-//   //check to see if a number has a duplicate
-//   let uniqueInt
-//   let duplicates = []
-// 
-//   for(i=0; i < array.length; i++){
-//     if(array[i] === i){
-//       duplicates.push(i)
-//     }
-//     else{
-//       uniqueInt = i
-//     }
-//   }
-//   console.log(duplicates)
-//   return uniqueInt
-// }
+// Example
+// Input: [4,1,2,1,2]
+// Output: 4
+
+let singleNumber = function(nums) {
+    let intCounter = {}
+
+    for(let int of nums){
+        intCounter[int] = intCounter[int] + 1 || 1
+    }
+
+    return Object.keys(intCounter).find(key => intCounter[key] === 1)
+};
